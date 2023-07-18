7f1bdda1aa6ee984392e841ec19fb362ba5497d7
Fernando Marquez <fernandomarquez@usc-eduroam-upc-new235.usc.edu>
2023-07-18 10:10:40 -0700

    yo

---
+++ api/index.js
@@ -0,0 +1,105 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const { default: mongoose } = require('mongoose');
+const User= require('./models/User');
+const Post= require('./models/Post');
+const bcrypt= require('bcryptjs');
+const jwt= require('jsonwebtoken');
+const cookieParser= require('cookie-parser');
+const multer = require('multer');
+const uploadMiddle = multer({dest:'uploads/'});
+const fs= require('fs');
+const path = require('path');
+
+const salt= bcrypt.genSaltSync(10);
+const secret= 'kjsdfkjsebfq34';
+
+app.use(cors({credentials:true, origin:'http://localhost:3000'}));
+app.use(express.json());
+app.use(cookieParser())
+app.use('/uploads', express.static(__dirname+ '/uploads'));
+
+
+mongoose.connect('mongodb+srv://fmarq:McDhS3EEJZsrzX35@blog.nkkx6wk.mongodb.net/?retryWrites=true&w=majority')
+
+app.post('/register', async(req,res)=>{
+    const {username,password}= req.body;
+    try{
+        const userDoc = await User.create({
+            username,
+            password: bcrypt.hashSync(password,salt),
+        });
+        res.json(userDoc);
+    }
+    catch(e){
+        res.status(400).json(e)
+    }
+});
+
+app.post('/login', async(req,res)=>{
+    const {username,password}= req.body;
+    const userDoc= await User.findOne({username});
+    const passOk= bcrypt.compareSync(password,userDoc.password);
+    if (passOk){
+        //log
+        jwt.sign({username,id:userDoc._id}, secret, {},(err,token)=>{
+            if(err) throw err;
+            res.cookie('token',token).json({
+                id:userDoc._id,
+                username,
+            });
+        });
+    }
+    else{
+        res.status(400).json('wrong login');
+    }
+});
+
+app.get('/profile', (req,res)=>{
+    const {token}= req.cookies;
+    jwt.verify(token, secret, {},(err,info)=>{
+        if (err) throw err;
+        res.json(info);
+    }) 
+    res.json(req.cookies);
+});
+
+app.post('/logout', (req,res)=>{
+    res.cookie('token','').json('ok')
+});
+
+app.post('/post',uploadMiddle.single('file'),async(req,res)=>{
+    const {originalname,path} = req.file;
+    const parts = originalname.split('');
+    const ext = parts[parts.length-1];
+    const newPath= path+'.'+ext;
+    fs.renameSync(path, newPath);
+
+    const {token}= req.cookies
+    jwt.verify(token, secret, {},async(err,info)=>{
+        if (err) throw err;
+        const {title,summary,content}= req.body;
+        const postDoc= await Post.create({
+            title,
+            summary,
+            content,
+            cover:newPath,
+            author: info.id,
+    });
+        res.json(postDoc);
+    });
+
+    
+});
+
+app.get('/post', async(req,res)=>{
+    res.json(
+        await Post.find()
+        .populate('author',['username'])
+        .sort({createdAt: -1})
+        .limit(20)
+        );
+});
+
+app.listen(4000);
